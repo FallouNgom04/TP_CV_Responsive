75b83dc1cb031182c40a9e24f9becc43d93cf7da
Fallou Ngom <sfngom@ept.sn>
2025-12-31 19:12:27 +0000

    Partie5 - Création des composants Sidebar et MainContent

--- cv_bootstrap/src/app/app.ts
+++ cv_bootstrap/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { Entete } from './entete/entete';
+import { Sidebar } from './sidebar/sidebar';
+import { MainContent } from './main-content/main-content';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Entete],
+  imports: [Entete, Sidebar, MainContent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ cv_bootstrap/src/app/main-content/main-content.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-content-cv',
+  imports: [],
+  templateUrl: './main-content.html',
+  styleUrl: './main-content.scss',
+})
+export class MainContent {
+
+}
